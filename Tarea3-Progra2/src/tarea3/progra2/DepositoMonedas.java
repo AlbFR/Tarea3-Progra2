29f611ea15382b42c2a8415f6275cf4688ff0c68
AlbFR <albertoferrada20@gmail.com>
2022-11-15 10:27:36 -0300

    Code for painting Deposito, not yet successfull

--- Tarea3-Progra2/src/tarea3/progra2/DepositoMonedas.java
+++ Tarea3-Progra2/src/tarea3/progra2/DepositoMonedas.java
@@ -1,15 +1,31 @@
 package tarea3.progra2;
+
 import java.util.ArrayList;
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class DepositoMonedas{
     private ArrayList<Moneda> a;
+    private BufferedImage img;
 
     public DepositoMonedas(){
         a = new ArrayList<Moneda>();
+        try {
+            img = ImageIO.read(getClass().getResource("assets/deposito_monedas.png"));
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     public void addMoneda(Moneda b){
         a.add(b);
+    }
+
+    public void paint(Graphics g) {
+        g.drawImage(this.img, 200, 100, null);
     }
 
     public Moneda getMoneda(){
